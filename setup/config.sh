f10c7bb7203651f716ca240ab876ba6b36528acc
kappa331 <wawata1qwerty@gmail.com>
2024-12-15 11:19:48 +0900

    setupフォルダーとりあえず完成

--- setup/config.sh
+++ setup/config.sh
@@ -5,5 +5,4 @@ export RAILS_APP_NAME=myapp
 export RAILS_PORT=3000
 export RAILS_VERSION="'~> 7.2.0'"
 export DATABASE_NAME=postgresql
-export POSTGRES_USERNAME=postgresql
 export POSTGRES_PASSWORD=password
